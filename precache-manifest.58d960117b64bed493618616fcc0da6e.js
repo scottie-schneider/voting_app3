258cf52d7c5d4ab2979caf63b4d364fb1a067c7e
Scottie Schneider <scott@laclave.co>
2019-01-13 16:37:17 -0500

    Updates

---
+++ precache-manifest.58d960117b64bed493618616fcc0da6e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/voting_app3/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "11c8da2c332a33084280",
+    "url": "/voting_app3/static/js/runtime~main.11c8da2c.js"
+  },
+  {
+    "revision": "5d2d3bed7e7b29984ab9",
+    "url": "/voting_app3/static/js/main.5d2d3bed.chunk.js"
+  },
+  {
+    "revision": "f739feb7d61c48a46bed",
+    "url": "/voting_app3/static/js/1.f739feb7.chunk.js"
+  },
+  {
+    "revision": "5d2d3bed7e7b29984ab9",
+    "url": "/voting_app3/static/css/main.97569232.chunk.css"
+  },
+  {
+    "revision": "cb39ce43686774b7e8c2dde859e02438",
+    "url": "/voting_app3/index.html"
+  }
+];
